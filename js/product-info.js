ea447b20074a1ad455cb41dd5530b5a747a61031
Sandra Martinez <Sandramilenamartinezarrieta@hotmail.com>
2022-09-22 07:12:34 -0300

    info organizada
    organizacion info style

--- js/product-info.js
+++ js/product-info.js
@@ -7,7 +7,22 @@ function carsproductinfo(Array) {
     }
     let productinfo = Array[i];
     htmlContentToAppend +=
-       `<div> <h4> <strong> mama</strong>  </h4>
+       `<div> <div class="card border-light mb-3" style="max-width: 50rem;">
+       <div class="card-header bg-transparent "> <h1>Oso de peluche<h1/></div>
+       <div class="card-body ">
+         <h5 class="card-title"><strong>Success card title</strong></h5>
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <h5 class="card-title"><strong>Success card title</strong></h5>
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <h5 class="card-title"><strong>Success card title</strong></h5>
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <h5 class="card-title"><strong>Success card title</strong></h5>
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <h5 class="card-title"><strong>Success card title</strong></h5>
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         </div>
+       <div class="card-footer border-light mb-3 bg-transparent">Footer</div>
+     </div>
        </div> 
        <div></div>`          
     document.getElementById('info-container').innerHTML = htmlContentToAppend;
