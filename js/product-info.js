a00c66ebd263b531b108bea2de03ac828b97e8ec
Sandra Martinez <Sandramilenamartinezarrieta@hotmail.com>
2022-10-08 12:07:04 -0300

    provando carrusel
    insertado en html

--- js/product-info.js
+++ js/product-info.js
@@ -1,11 +1,13 @@
 let Arrayproductos = [];
 
+
+
 function carsproductinfo(Array) {
-    let htmlContentToAppend = "";
-    images = Array.images
-    
-    htmlContentToAppend +=
-        `<div> <div class="card border-light mb-3" style="max-width: 50rem;">
+  let htmlContentToAppend = "";
+  images = Array.images
+
+  htmlContentToAppend +=
+    `<div> <div class="card border-light mb-3" style="max-width: 50rem;">
        <div class="card-header bg-transparent "> <h1>${Array.name}<h1/></div>
        <div class="card-body ">
          <h5 class="card-title"><strong>Precio</strong></h5>
@@ -37,66 +39,67 @@ function carsproductinfo(Array) {
          </div>
       
      `
-    document.getElementById('info-container').innerHTML = htmlContentToAppend;
+  document.getElementById('info-container').innerHTML = htmlContentToAppend;
 }
 
 //funcion para comentar
 function carsproduccoment(Arraycomment) {
-let  htmlContentToAppend = "";
-let htmlContentToAppend2 = "";
-let htmlContentToAppend3 = "";
-  
-   //seccion comentarios
+  let htmlContentToAppend = "";
+  let htmlContentToAppend2 = "";
+  let htmlContentToAppend3 = "";
+  console.log(Arraycomment);
+  //seccion comentarios
   htmlContentToAppend +=
-      `
+    `
      <div class="card-footer border-light mb-3 bg-transparent">
      <div><h3>Comentarios</h3>
           `
-  
-  
-//ingreso de cada comentario del api
-  for(let i = 0; i < Arraycomment.length; i++){
+
+
+  //ingreso de cada comentario del api
+  for (let i = 0; i < Arraycomment.length; i++) {
     let comment = Arraycomment[i];
     //if para puntuar cada comentario
-    if(comment.score==""){
-      comment.score="provando el if";}
-      else if(comment.score=="1"){
-        comment.score= `<span class="fa fa-star checked"></span>
+    if (comment.score == "") {
+      comment.score = "provando el if";
+    }
+    else if (comment.score == "1") {
+      comment.score = `<span class="fa fa-star checked"></span>
                         <span class="fa fa-star" ></span>
                         <span class="fa fa-star" ></span>
                         <span class="fa fa-star"></span>
                         <span class="fa fa-star"></span>`
-      }
-      else if(comment.score=="3"){
-        comment.score= `<span class="fa fa-star checked"></span>
+    }
+    else if (comment.score == "3") {
+      comment.score = `<span class="fa fa-star checked"></span>
                         <span class="fa fa-star checked"></span>
                         <span class="fa fa-star"></span>
                         <span class="fa fa-star"></span>
                         <span class="fa fa-star"></span>`
-      }
-      else if(comment.score=="3"){
-        comment.score= `<span class="fa fa-star checked"></span>
+    }
+    else if (comment.score == "3") {
+      comment.score = `<span class="fa fa-star checked"></span>
                         <span class="fa fa-star checked"></span>
                         <span class="fa fa-star checked"></span>
                         <span class="fa fa-star"></span>
                         <span class="fa fa-star"></span>`
-      }
-      else if(comment.score=="4"){
-        comment.score= `<span class="fa fa-star checked"></span>
+    }
+    else if (comment.score == "4") {
+      comment.score = `<span class="fa fa-star checked"></span>
                         <span class="fa fa-star checked"></span>
                         <span class="fa fa-star checked"></span>
                         <span class="fa fa-star checked"></span>
                         <span class="fa fa-star"></span>`
-      }
-   
-      else if(comment.score=="5"){
-        comment.score= `<span class="fa fa-star checked"></span>
+    }
+
+    else if (comment.score == "5") {
+      comment.score = `<span class="fa fa-star checked"></span>
                         <span class="fa fa-star checked"></span>
                         <span class="fa fa-star checked"></span>
                         <span class="fa fa-star checked"></span>
                         <span class="fa fa-star checked"></span>`
-      }
-      //apend del comentario
+    }
+    //apend del comentario
     htmlContentToAppend2 +=
       `            
               
@@ -115,12 +118,12 @@ let htmlContentToAppend3 = "";
           
         `
 
-     
-      }
 
- // ingreso del comentario individual 
- htmlContentToAppend3 =
-        `<br>
+  }
+
+  // ingreso del comentario individual 
+  htmlContentToAppend3 =
+    `<br>
      </div>
      <div> <h3>Comentar</h3>
      <div class="mb-3">
@@ -149,10 +152,10 @@ let htmlContentToAppend3 = "";
      
      </div>`
 
-     let seccioncoment =htmlContentToAppend;
-     let everycoment = htmlContentToAppend2;
-     let comentinsert = htmlContentToAppend3;
-  document.getElementById('info-comment').innerHTML = seccioncoment+everycoment+comentinsert;
+  let seccioncoment = htmlContentToAppend;
+  let everycoment = htmlContentToAppend2;
+  let comentinsert = htmlContentToAppend3;
+  document.getElementById('info-comment').innerHTML = seccioncoment + everycoment + comentinsert;
 }
 
 
@@ -161,53 +164,73 @@ let htmlContentToAppend3 = "";
 
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCT_INFO_URL + localStorage.prod + EXT_TYPE)
-        .then(function (resultObj) {
-            if (resultObj.status === "ok") {
+  getJSONData(PRODUCT_INFO_URL + localStorage.prod + EXT_TYPE)
+    .then(function (resultObj) {
+      if (resultObj.status === "ok") {
 
-                Arrayproductos = resultObj.data
-                carsproductinfo(Arrayproductos)
-            }
-        })
-        getJSONData(PRODUCT_INFO_COMMENTS_URL + localStorage.prod + EXT_TYPE)
-        .then(function (resultObj) {
-            if (resultObj.status === "ok") {
+        Arrayproductos = resultObj.data
+        carsproductinfo(Arrayproductos)
+        console.log(Arrayproductos)
+      }
+    })
+  getJSONData(PRODUCT_INFO_COMMENTS_URL + localStorage.prod + EXT_TYPE)
+    .then(function (resultObj) {
+      if (resultObj.status === "ok") {
 
-              Arraycomment = resultObj.data
-              carsproduccoment(Arraycomment)
-            }
-            
-        }) 
-        getJSONData(PRODUCT_INFO_COMMENTS_URL + localStorage.catID + EXT_TYPE)
-        .then(function (resultObj) {
-            if (resultObj.status === "ok") {
+        let Arraycomment = resultObj.data
+        carsproduccoment(Arraycomment)
+        console.log(Arraycomment)
+      }
 
-              Arraycomment = resultObj.data
-              productrelativ(Arraycomment)
-            }
-            
-        }) 
+    })
+  getJSONData(PRODUCTS_URL + localStorage.catID + EXT_TYPE)
+    .then(function (resultObj) {
+      if (resultObj.status === "ok") {
+
+        let listacart = resultObj.data
+        console.log(listacart)
+        productrelativ(listacart)
+        console.log(listacart)
+      }
+
+    })
 })
 
-window.onload += function productrelativ(lista) {
-  let htmlContentToAppend = "";
+// funcion de carga para ingresar el carrusel de fotos
+function productrelativ(listacart) {
+  let htmlAppend1 = "";
+  let htmlAppend2 = "";
 
-    for(let i = 0; i < lista.length; i++){
-        let category = lista[i];
 
-        
-            htmlContentToAppend += `
-            <div class="carousel-item">
-  <img src="..." alt="...">
-  <div class="carousel-caption d-none d-md-block">
-    <h5>...</h5>
-    <p>...</p>
-  </div>
-</div>
-            `
-        }
+  let Arraylistacart = listacart.products
+  console.log(Arraylistacart)
+
+  
+   // ingreso del for en cada imagen y cada boton en las variables 
+     for (let i = 0; i < Arraylistacart.length; i++) {
+    let category = Arraylistacart[i];
+    console.log(category);
+    console.log(i)
+
+    htmlAppend1 += `<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="${i}" aria-label="Slide ${i+=1}"></button> `
+    htmlAppend2 += `
+    <div class="carousel-item active" data-bs-interval="2000">
+    <img src="${category.image}" class="d-block w-100" alt="${category.name}">
+    <div class="carousel-caption d-none d-md-block">
+      <h5 class="text-white bg-dark">First slide label</h5>
+      <p class="text-white bg-dark">Some representative placeholder content for the first slide.</p>
+    </div>
+  </div> `
+  }
+console.log(htmlAppend2);
+console.log(htmlAppend1);
+document.getElementById("info-carrusel").innerHTML += htmlAppend1
+document.getElementById("info-carrusel2").innerHTML += htmlAppend2
+// document.getElementsByTagName("info-carrusel3").data += htmlAppend2
+
 
 }
+
 
 
 
