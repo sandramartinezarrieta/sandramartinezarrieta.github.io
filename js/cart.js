43df2b5caf2d7612b3d1640c014b4625d0c058b2
Sandra Martinez <Sandramilenamartinezarrieta@hotmail.com>
2022-10-30 22:29:48 -0300

    entrega no. 5
    el cambio en el cart

--- js/cart.js
+++ js/cart.js
@@ -4,30 +4,34 @@ URL = CART_INFO_URL + "25801" + EXT_TYPE
 let cantidad = document.getElementById("cant")
 const costo = document.getElementById("cost")
 let subtotal= document.getElementById("subtotal")
+let image = document.getElementById("image")
 let valorDatoCosto=0;
+let valorCosto=0;
 // funcion para cargar los datos al cart
-function carga() {
-    fetch(URL)
+async function carga(url){
+    await fetch(url)
         .then((response) => response.json())
         .then((data) => {
             const datos=data.articles
             const nombre= document.getElementById("name")
             nombre.innerHTML= datos[0].name
-            valorDatoCosto += datos[0].unitCost
+            valorCosto += datos[0].unitCost
             costo.innerHTML= datos[0].unitCost
             cantidad.value = datos[0].count
             console.log(datos[0]);
-            subtotal.innerHTML= datos[0].currency +" "+ 1*valorDatoCosto
-
+            subtotal.innerHTML= datos[0].currency +" "+ 1*valorCosto
+            console.log(valorCosto)
+            image.innerHTML= `<img  src="${datos[0].image}" alt="" class="col-12 w-10">`
         })
-     return valorDatoCosto
+return valorDatoCosto += valorCosto
+     
 }
 
-
+console.log(valorDatoCosto)
 
 // funcion para cargar el fetch al cart cuando se carga el DOM
 document.addEventListener("DOMContentLoaded", function cargahtml() {
-    carga();
+  carga(URL);
     console.log(valorDatoCosto)
     // funcion para cambiar en tiempo real el valor del subtotal
     cantidad.addEventListener("change", function (e) {
